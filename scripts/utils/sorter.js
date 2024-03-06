eadd0418c351fd67e53f9c8f8a31c9081c6b1c9a
Kevin-Jerbic <94851418+Kevin-Jerbic@users.noreply.github.com>
2024-03-07 00:36:19 +0100

    Ajout de la fonctionnalite de tri par popularite, date et titre

---
+++ scripts/utils/sorter.js
@@ -0,0 +1,37 @@
+function sortBy(selectValue, medias) {
+    let arr = medias.slice();
+
+    switch (selectValue) {
+        case "Popularité":
+            arr.sort((a, b) => b.likes - a.likes);
+            break;
+        case "Date":
+            arr.sort((a, b) => {
+                const dateA = new Date(a.date);
+                const dateB = new Date(b.date);
+                return dateA - dateB;
+            });
+            break;
+        case "Titre":
+            arr.sort((a, b) => a.title.localeCompare(b.title));
+            break;
+        default:
+            break;
+    }
+
+    return arr;
+}
+
+function onSelectChange(selectElement, instancePhotograph) {
+    let $parentContainer = document.querySelector(".photograph_medias_section");
+    let $articles = document.querySelectorAll(".photograph_media_item");
+
+    // Remove all existing articles from the media section
+    $articles.forEach(article => {
+        article.remove();
+    });
+    // Creates new articles based on the selection
+    instancePhotograph.createPhotographMediaContent($parentContainer, selectElement);
+}
+
+export { sortBy, onSelectChange };
