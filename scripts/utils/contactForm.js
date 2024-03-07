41813f1413c3cc9b7d2e3c6f5b80274dbf1bd759
Kevin-Jerbic <94851418+Kevin-Jerbic@users.noreply.github.com>
2024-03-07 01:31:09 +0100

    Ajout modal contact + navigation au clavier

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,48 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    const $modalWrapper = document.getElementById("contact_modal");
+    $modalWrapper.style.display = "block";
+    const $contactModal = document.querySelector(".modal");
+    document.body.classList.add("modal-open");
+
+    if ($contactModal) {
+        $contactModal.focus();
+        focusContactModal($contactModal);
+    }
+    $contactModal.addEventListener("keyup", onKeyUpContact);
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    document.body.classList.remove("modal-open");
+    document.removeEventListener("keyup", onKeyUpContact);
 }
+
+// Sets up keyboard navigation within a form modal.
+function focusContactModal(form) {
+    const $contactModal = form;
+
+    $contactModal.addEventListener("keydown", event => {
+        if (event.key === "Tab") {
+            const focusableElements = $contactModal.querySelectorAll("input, textarea, button");
+
+            const firstElement = focusableElements[0];
+            const lastElement = focusableElements[focusableElements.length - 1];
+
+            if (!event.shiftKey && document.activeElement === lastElement) {
+                event.preventDefault();
+                firstElement.focus();
+            } else if (event.shiftKey && document.activeElement === firstElement) {
+                event.preventDefault();
+                lastElement.focus();
+            }
+        }
+    });
+}
+function onKeyUpContact(e) {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+}
+
+export { displayModal, closeModal, focusContactModal, onKeyUpContact };
