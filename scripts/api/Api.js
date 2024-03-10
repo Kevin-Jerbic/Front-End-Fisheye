0692635abb46d81331ba41ac5f6cf57cff2254be
Kevin-Jerbic <94851418+Kevin-Jerbic@users.noreply.github.com>
2024-03-10 01:48:16 +0100

    Tentative résolution de bug

--- scripts/api/Api.js
+++ scripts/api/Api.js
@@ -6,8 +6,8 @@ class Api {
 
     async get() {
         return fetch(this.url)
-            .then(res => res.json())
-            .catch(err => console.log("An error occurs", err));
+            .then((res) => res.json())
+            .catch((err) => console.log("An error occurs", err));
     }
 }
 
